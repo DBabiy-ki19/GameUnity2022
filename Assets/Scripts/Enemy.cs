1caeb55cf40cfc1cb5da1f0add53a3d382ca6a2b
DBabiy-ki19 <dimascelecto@gmail.com>
2022-05-31 20:35:12 +0700

    NawMesh + ИИ врага

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
+    public Transform playerTr;
+    private NavMeshAgent agent;
     public int maxHealth = 100;
     private int currentHealt;
 
     private void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
         currentHealt = maxHealth;
+        
     }
 
+    private void FixedUpdate()
+    {
+        agent.SetDestination(playerTr.position);
+    }
     public void TakeDamage(int damage)
     {
         currentHealt -= damage;
